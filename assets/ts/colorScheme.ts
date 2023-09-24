b57b8e7861f62fad2adb15bfe69d95dfc2205263
ETOgaosion <gaoziyuan19@mails.ucas.ac.cn>
2023-09-24 12:28:18 +0800

    enable right widgets, refactor dark mode

--- assets/ts/colorScheme.ts
+++ assets/ts/colorScheme.ts
@@ -1,13 +1,23 @@
 type colorScheme = 'light' | 'dark' | 'auto';
+type functionCallback = (e: MediaQueryListEvent) => void;
 
 class StackColorScheme {
     private localStorageKey = 'StackColorScheme';
     private currentScheme: colorScheme;
     private systemPreferScheme: colorScheme;
+    private eventListener: functionCallback;
 
     constructor(toggleEl: HTMLElement) {
-        this.bindMatchMedia();
         this.currentScheme = this.getSavedScheme();
+
+        var elem = <HTMLElement>document.getElementsByClassName('icon-tabler-toggle-right')[0];
+        elem.style.display = 'none';
+        elem = <HTMLElement>document.getElementsByClassName('icon-tabler-toggle-center')[0];
+        elem.style.display = 'none';
+        elem = document.getElementById('dark-mode-auto');
+        elem.style.display = 'none';
+        elem = document.getElementById('dark-mode-dark');
+        elem.style.display = 'none';
 
         this.dispatchEvent(document.documentElement.dataset.scheme as colorScheme);
 
@@ -16,6 +26,18 @@ class StackColorScheme {
 
         if (document.body.style.transition == '')
             document.body.style.setProperty('transition', 'background-color .3s ease');
+        
+        this.eventListener = function(e: MediaQueryListEvent) : void {
+            if (this.currentScheme == 'auto') {
+                if (e.matches) {
+                    this.systemPreferScheme = 'dark';
+                }
+                else {
+                    this.systemPreferScheme = 'light';
+                }
+                this.setBodyClass();
+            }
+        }
     }
 
     private saveScheme() {
@@ -24,20 +46,44 @@ class StackColorScheme {
 
     private bindClick(toggleEl: HTMLElement) {
         toggleEl.addEventListener('click', (e) => {
-            if (this.isDark()) {
-                /// Disable dark mode
+            var elem_left_display = 'none';
+            var elem_center_display = 'none';
+            var elem_right_display = 'none';
+            var elem_light_display = 'none';
+            var elem_auto_display = 'none';
+            var elem_dark_display = 'none';
+            if (this.currentScheme == 'dark') {
                 this.currentScheme = 'light';
+                elem_left_display = 'block';
+                elem_light_display = 'block';
+            }
+            else if (this.currentScheme == 'auto') {
+                this.currentScheme = 'dark';
+                this.dispatchMatchMedia();
+                elem_right_display = 'block';
+                elem_dark_display = 'block';
             }
             else {
-                this.currentScheme = 'dark';
+                this.currentScheme = 'auto';
+                this.bindMatchMedia();
+                elem_center_display = 'block';
+                elem_auto_display = 'block';
             }
+
+            var elem = <HTMLElement>document.getElementsByClassName('icon-tabler-toggle-left')[0];
+            elem.style.display = elem_left_display;
+            var elem = <HTMLElement>document.getElementsByClassName('icon-tabler-toggle-center')[0];
+            elem.style.display = elem_center_display;
+            var elem = <HTMLElement>document.getElementsByClassName('icon-tabler-toggle-right')[0];
+            elem.style.display = elem_right_display;
+            elem = document.getElementById('dark-mode-light');
+            elem.style.display = elem_light_display;
+            elem = document.getElementById('dark-mode-auto');
+            elem.style.display = elem_auto_display;
+            elem = document.getElementById('dark-mode-dark');
+            elem.style.display = elem_dark_display;
 
             this.setBodyClass();
-
-            if (this.currentScheme == this.systemPreferScheme) {
-                /// Set to auto
-                this.currentScheme = 'auto';
-            }
 
             this.saveScheme();
         })
@@ -72,16 +118,14 @@ class StackColorScheme {
         else return 'auto';
     }
 
+
+
     private bindMatchMedia() {
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-            if (e.matches) {
-                this.systemPreferScheme = 'dark';
-            }
-            else {
-                this.systemPreferScheme = 'light';
-            }
-            this.setBodyClass();
-        });
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', this.eventListener);
+    }
+
+    private dispatchMatchMedia() {
+        window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', this.eventListener);
     }
 }
 
